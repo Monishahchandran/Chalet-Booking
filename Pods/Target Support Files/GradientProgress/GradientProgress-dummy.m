8ef542c6b8929683d90e76feb5c2960984d366c5
Visakhmp01 <visakhmp.srishti@gmail.com>
2021-06-04 21:14:22 +0530

    Delete all the stuff

--- Pods/Target Support Files/GradientProgress/GradientProgress-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GradientProgress : NSObject
-@end
-@implementation PodsDummy_GradientProgress
-@end
--- Pods/Target Support Files/Pods-AbyChaletApp/Pods-AbyChaletApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AbyChaletApp : NSObject
-@end
-@implementation PodsDummy_Pods_AbyChaletApp
-@end
